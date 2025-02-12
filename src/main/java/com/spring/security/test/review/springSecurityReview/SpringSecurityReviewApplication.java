5e95e3d953421c455716b1c61b429383e33536e4
golamyusuf <golam.yousuf@bjitgroup.com>
2025-02-12 17:31:53 +0600

    Initial commit

---
+++ src/main/java/com/spring/security/test/review/springSecurityReview/SpringSecurityReviewApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.test.review.springSecurityReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/security/test/review/springSecurityReview/SpringSecurityReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.test.review.springSecurityReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
